52ef680eca4e224bd0a54d1840bfc7734c7e7628
David Silverstone <dsilvers@usc.edu>
2018-03-25 10:55:51 -0700

    server stuff; network makefile

--- server/index.js
+++ server/index.js
@@ -1,9 +1,43 @@
 const http = require('http')
+const fs = require('fs')
 const port = 3000
+//var app = express.createServer(express.logger());
+//app.use(express.bodyParser());
+
+const original_response = {
+  number_of_people: 4,
+  vibe: "lit"
+};
+
+function errorHandler (err) {
+  console.log("fuck", err);
+}
 
 const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
+  const { method, url } = request;
+  console.log(method, url)
+  let my_response = Object.assign({}, original_response);
+  let body = [];
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+    console.log(body);
+    try {
+      body = JSON.parse(body);
+      my_response.number_of_people = body.number_of_people;
+    } catch (err) {
+      errorHandler(err);
+      console.log("Could not parse JSON");
+    }
+    console.log(JSON.stringify(my_response));
+    response.write(JSON.stringify(my_response));
+    response.end();
+  }).on('error', errorHandler);
+  response.writeHead(200,
+    { 'Content-Type': 'application/json' });
+  //response.write(JSON.stringify(my_response));
+  //response.end('Hello Node.js Server!')
 }
 
 const server = http.createServer(requestHandler)
